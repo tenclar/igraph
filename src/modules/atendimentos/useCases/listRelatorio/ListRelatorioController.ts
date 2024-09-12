01ac09554efef4ead541d86689d56ca4a459ceee
Andreysson Araújo dos Santos <andreysson.santos@ac.gov.br>
2024-09-12 10:25:40 -0400

    setupDerELATORIOS

--- src/modules/atendimentos/useCases/listRelatorio/ListRelatorioController.ts
+++ src/modules/atendimentos/useCases/listRelatorio/ListRelatorioController.ts
@@ -3,17 +3,24 @@ import { container } from "tsyringe";
 import { ListRelatorioUseCase } from "./ListRelatorioUseCase";
 
 class ListRelatorioController {
-  async handle(request: Request, response: Response): Promise<Response> {
-    const { mes, ano, unidade_id } = request.query;
+  handle(arg0: string, handle: any) {
+      throw new Error("Method not implemented.");
+  }
+  async handleByMesAnoUnidade(request: Request, response: Response): Promise<Response> {
+    // Extraindo os parâmetros mes, ano e unidade_id da URL
+    const { mes, ano, unidade_id } = request.params;
 
+    // Resolvendo o caso de uso ListRelatorioUseCase usando o container do tsyringe
     const listRelatorioUseCase = container.resolve(ListRelatorioUseCase);
 
+    // Executando o caso de uso com os parâmetros passados
     const relatorio = await listRelatorioUseCase.execute({
-      mes: mes ? Number(mes) : undefined,
-      ano: ano ? Number(ano) : undefined,
-      unidade_id: unidade_id ? Number(unidade_id) : undefined,
+      mes: Number(mes),
+      ano: Number(ano),
+      unidade_id: Number(unidade_id),
     });
 
+    // Retornando a resposta JSON com o relatório
     return response.json(relatorio);
   }
 
@@ -36,7 +43,6 @@ class ListRelatorioController {
     const relatorio = await listRelatorioUseCase.execute({ servico_id: Number(servico_id) });
     return response.json(relatorio);
   }
-
 }
 
 export { ListRelatorioController };
--- src/routes/relatorio.routes.ts
+++ src/routes/relatorio.routes.ts
@@ -6,6 +6,7 @@ const relatorioController = new ListRelatorioController();
 
 // Define a rota de relatórios com possíveis filtros como query parameters
 relatorioRoutes.get("/", relatorioController.handle);
+relatorioRoutes.get("/:mes/:ano/:unidade_id", relatorioController.handleByMesAnoUnidade);
 relatorioRoutes.get("/unidades/:unidade_id", relatorioController.handleByUnidade);
 relatorioRoutes.get("/servicos/:servico_id", relatorioController.handleByServico);
 
